97dc123415a1d6d653895d7e20991704b93757fe
satriaersan <satriaersanarendra@gmail.com>
2025-02-27 21:59:32 +0700

    POS_Soal-Praktikum_Jobsheet2

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route untuk halaman Home
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Route untuk halaman Products dengan prefix 'category'
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+});
+
+// Route untuk halaman User dengan parameter id
+Route::get('/user/{id}', [UserController::class, 'profile'])->name('user.profile');
+
+// Route untuk halaman Penjualan (POS)
+Route::get('/sales', [SalesController::class, 'index'])->name('sales');
